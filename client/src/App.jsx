8ba0781b68234a33bfbae4d34662d0f1054f3afa
Darshan Badiger <140099027+dbadiger@users.noreply.github.com>
2024-12-07 14:27:32 +0000

    Pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import BuyCredit from './pages/BuyCredit'
+import Result from './pages/Result'
 
 const App = () => {
   return (
-    <div>
-      ArtLnes
+    <div className="">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/buy" element={<BuyCredit />} />
+      </Routes>
+
     </div>
   )
 }
