3f6b5ccf354dd4cfc824e3b8500e8044822f3690
Darshan Badiger <140099027+dbadiger@users.noreply.github.com>
2024-12-24 03:10:24 +0000

    backend conrollers,api completed

---
+++ server/routes/userRoute.js
@@ -0,0 +1,15 @@
+import express from "express"
+import {registerUser,loginUser, userCredit } from "../controllers/userController.js"
+import userAuth from "../middlewares/auth.js";
+
+const userRouter = express.Router();
+
+userRouter.post("/register", registerUser)
+userRouter.post("/login", loginUser)
+userRouter.post("/credit",userAuth, userCredit)
+
+export default userRouter;
+
+//localhost:4000/api/user/register
+//localhost:4000/api/user/login
+//localhost:4000/api/user/credit
